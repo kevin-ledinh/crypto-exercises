aeb9952512e9f97f720a0064797b73aa198b335a
kevin-ledinh <kevin.ledinh@hotmail.com>
2016-05-02 12:47:34 +0100

    commiting skeleton code for week 2

---
+++ wk2_block_cipher_modes/src/wk2_block_cipher_modes.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : wk2_block_cipher_modes.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
