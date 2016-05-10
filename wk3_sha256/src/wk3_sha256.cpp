6f01caf9c6b1fae472d3086bb5f12ea705e98871
Kevin-Le <kevin.ledinh@users.noreply.github.com>
2016-05-10 21:05:58 +0100

    Nearly there, do multi hash update

--- wk3_sha256/src/wk3_sha256.cpp
+++ wk3_sha256/src/wk3_sha256.cpp
@@ -7,9 +7,106 @@
 //============================================================================
 
 #include <iostream>
+#include <fstream>
+
+#include <string>
+using std::string;
+
+#include "filters.h"
+
+#include "hex.h"
+using CryptoPP::HexEncoder;
+using CryptoPP::HexDecoder;
+
+#include "sha.h"
+using CryptoPP::SHA256;
+
 using namespace std;
+using namespace CryptoPP;
+
+// Prototypes
+void GetSHA256(CryptoPP::HashTransformation& hash, char * message, int msg_size, byte * SHAOutput, int OutputSize);
+void DisplayByteArray( const string ArrayName, byte * ByteArray, int ArraySize );
+
 
 int main() {
+	// Global variables
+	SHA256 sha;
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	ifstream myfile ("sample1.mp4", ios::binary|ios::ate);
+
+	int FileSize;
+	streampos currentRdPos;
+	int BlockSize = 1024; // 1kB
+	int NumberOfBlocks = 0;
+	int LastBlockSize = 0;
+
+	if( myfile.is_open() )
+	{
+		char message[ 1024 + SHA256::DIGESTSIZE ] = {0};
+		byte signature[ SHA256::DIGESTSIZE ];
+
+		// Get File parameters
+		FileSize = myfile.tellg();
+		NumberOfBlocks = ( FileSize / BlockSize ) + 1;
+		LastBlockSize = FileSize % BlockSize;
+		cout << "File size is: " << FileSize << " bytes.\n";
+		cout << "Number of blocks are: " << NumberOfBlocks << endl;
+		cout << "Last block size is: " << LastBlockSize << endl;
+
+		// Get the SHA256 of the last block first
+		currentRdPos = FileSize - LastBlockSize;
+		myfile.seekg (currentRdPos, ios::beg);
+		myfile.read(message, LastBlockSize); // break here OMG
+		cout << "Current file pos: " << myfile.tellg() << endl;
+
+		GetSHA256(sha, message , LastBlockSize , signature , SHA256::DIGESTSIZE );
+		DisplayByteArray( "First hash: ", signature, SHA256::DIGESTSIZE);
+
+		// Calculate SHA256 of the rest of the file
+		// Get 1 block at a time and calculate SHA256 of one block
+		for(int i = 1; i < NumberOfBlocks; i++)
+		{
+			// Read the next 1kB of data
+			currentRdPos = FileSize - ( BlockSize * i );
+			myfile.seekg (currentRdPos, ios::beg);
+			myfile.read(message, currentRdPos);
+
+			// Append the hash of the last block to the current block
+			GetSHA256(sha, message , BlockSize , signature , SHA256::DIGESTSIZE );
+
+			// Get the hash of the current data
+
+		}
+		//print out the hash of the first block here
+
+	}
+
+	myfile.close();
 	return 0;
+}
+
+void GetSHA256(CryptoPP::HashTransformation& hash, char * message, int msg_size, byte * PreHash, byte * SHAOutput, int OutputSize)
+{
+	byte digest[ SHA256::DIGESTSIZE ];
+
+	hash.CalculateDigest( digest, ( byte * ) message, msg_size );
+
+	if (OutputSize == SHA256::DIGESTSIZE )
+	{
+		memcpy( SHAOutput , digest , OutputSize );
+	}
+
+}
+
+void DisplayByteArray( const string ArrayName, byte * ByteArray, int ArraySize )
+{
+	string SHAOutputStr;
+	SHAOutputStr.clear();
+	StringSource(ByteArray, ArraySize, true,
+		new HexEncoder(
+			new StringSink(SHAOutputStr)
+		) // HexEncoder
+	); // StringSource
+	cout << ArrayName << SHAOutputStr << endl;
 }
