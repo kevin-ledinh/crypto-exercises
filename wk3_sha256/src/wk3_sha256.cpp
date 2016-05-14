260f80cfaa457c8f00aecfb3dc496d94a5c88c70
Kevin-Le <kevin.ledinh@users.noreply.github.com>
2016-05-14 13:17:33 +0100

    Assignment done

--- wk3_sha256/src/wk3_sha256.cpp
+++ wk3_sha256/src/wk3_sha256.cpp
@@ -29,6 +29,10 @@ void GetSHA256(CryptoPP::HashTransformation& hash, char * message, int msg_size,
 void DisplayByteArray( const string ArrayName, byte * ByteArray, int ArraySize );
 
 
+const int BlockSize = 1024; // 1kB
+static byte message[ BlockSize ] = {0};
+static byte signature[ SHA256::DIGESTSIZE ] = {0};
+
 int main() {
 	// Global variables
 	SHA256 sha;
@@ -37,18 +41,15 @@ int main() {
 
 	int FileSize;
 	streampos currentRdPos;
-	int BlockSize = 1024; // 1kB
 	int NumberOfBlocks = 0;
 	int LastBlockSize = 0;
 
 	if( myfile.is_open() )
 	{
-		char message[ 1024 + SHA256::DIGESTSIZE ] = {0};
-		byte signature[ SHA256::DIGESTSIZE ];
 
 		// Get File parameters
 		FileSize = myfile.tellg();
-		NumberOfBlocks = ( FileSize / BlockSize ) + 1;
+		NumberOfBlocks = ( FileSize / BlockSize );
 		LastBlockSize = FileSize % BlockSize;
 		cout << "File size is: " << FileSize << " bytes.\n";
 		cout << "Number of blocks are: " << NumberOfBlocks << endl;
@@ -57,28 +58,32 @@ int main() {
 		// Get the SHA256 of the last block first
 		currentRdPos = FileSize - LastBlockSize;
 		myfile.seekg (currentRdPos, ios::beg);
-		myfile.read(message, LastBlockSize); // break here OMG
+		myfile.read( ( char * ) message, LastBlockSize); // break here OMG
 		cout << "Current file pos: " << myfile.tellg() << endl;
 
-		GetSHA256(sha, message , LastBlockSize , signature , SHA256::DIGESTSIZE );
+
+		sha.Update( (const byte * ) message, LastBlockSize);
+		sha.Final(signature);
 		DisplayByteArray( "First hash: ", signature, SHA256::DIGESTSIZE);
 
 		// Calculate SHA256 of the rest of the file
 		// Get 1 block at a time and calculate SHA256 of one block
-		for(int i = 1; i < NumberOfBlocks; i++)
+		for(int i = ( NumberOfBlocks - 1 ); i >= 0; i--)
 		{
 			// Read the next 1kB of data
-			currentRdPos = FileSize - ( BlockSize * i );
+			memset( message, 0x00, BlockSize );
+			currentRdPos = BlockSize * i;
 			myfile.seekg (currentRdPos, ios::beg);
-			myfile.read(message, currentRdPos);
-
-			// Append the hash of the last block to the current block
-			GetSHA256(sha, message , BlockSize , signature , SHA256::DIGESTSIZE );
+//			cout << "Current file pos: " << myfile.tellg() << endl;
+			myfile.read( ( char * ) message, BlockSize);
 
 			// Get the hash of the current data
-
+			sha.Update( (const byte * ) message, BlockSize);
+			sha.Update( (const byte * ) signature, SHA256::DIGESTSIZE);
+			sha.Final(signature);
 		}
 		//print out the hash of the first block here
+		DisplayByteArray( "Last hash: ", signature, SHA256::DIGESTSIZE);
 
 	}
 
